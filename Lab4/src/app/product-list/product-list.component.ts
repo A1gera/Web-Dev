16f375b992936bcacf825eb6d95d3829587e9f55
A1gera <a_baibakty@kbtu.kz>
2024-02-21 20:54:28 +0600

    lab4

---
+++ Lab4/src/app/product-list/product-list.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent {
+  products = [...products];
+
+  share(link: string) {
+    const shareMessage = `Product:  ${link}`;
+    const telegramLink = `https://t.me/share/url?url=${encodeURIComponent(shareMessage)}`;
+    window.location.href = telegramLink;
+  }
+
+  share_kaspi(link: string) {
+    window.location.href = link;
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
